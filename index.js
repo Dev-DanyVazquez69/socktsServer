5e866d686d1c479e528e7d4c718640805a7b3bcc
Dev-DanyVazquez69 <danyv7818@gmail.com>
2023-11-25 17:04:26 -0300

    mudança cors

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('express')()
 const server = require('http').createServer(app)
-const io = require('socket.io')(server, {cors: {origin: 'http://localhost:5173'}})
+const io = require('socket.io')(server, {cors: {origin: '*'}})
 
 const PORT = 3001
 
@@ -24,4 +24,4 @@ io.on('connection', socket => {
   })
 })
 
-server.listen(PORT, () => console.log('Server running...'))
+server.listen(PORT, () => console.log('Servidor Online'))
